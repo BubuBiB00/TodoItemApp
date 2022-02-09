668fda06d79ece75412b1b3241727fb3abe4f6db
BuildTools <unconfigured@null.spigotmc.org>
2022-02-09 16:57:06 +0100

    push

---
+++ static/todoitems.js
@@ -0,0 +1,30 @@
+function deleteItem(button)
+{
+    if(window.confirm("Wollen Sie das Item wirklich löschen"))
+    {
+        button.form.submit();
+    }
+}
+
+function showAllToasts()
+{
+    let toastElList = [].slice.call(document.querySelectorAll('.toast'));
+    let toastList = toastElList.map(function (toastEl)
+    { return new bootstrap.Toast(toastEl)
+    });
+
+    for (let toast of toastList)
+    {
+        toast.show();
+    }
+}
+
+showAllToasts();
+
+//function refresh_page()
+//{
+//    for (let i = 0, i < 1000, i++)
+//    {
+//        document.location.reload()
+//    }
+//}
